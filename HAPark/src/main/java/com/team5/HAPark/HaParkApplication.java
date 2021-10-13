11ba897df407f01a52b25b6a458aae5ffdd5fa71
Pankti Vyas <pn695271@dal.ca>
2021-10-13 11:14:57 -0700

    Initial Test added.

--- HAPark/src/main/java/com/team5/HAPark/HaParkApplication.java
+++ HAPark/src/main/java/com/team5/HAPark/HaParkApplication.java
@@ -2,12 +2,20 @@ package com.team5.HAPark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HaParkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HaParkApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
