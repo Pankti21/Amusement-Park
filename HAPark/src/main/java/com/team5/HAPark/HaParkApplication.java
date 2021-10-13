f62ea25d7c3cf3d783cfbf370cb9ea3098dec9f2
Pankti Vyas <pn695271@dal.ca>
2021-10-13 10:54:26 -0700

    Initialized Spring Boot Project.

---
+++ HAPark/src/main/java/com/team5/HAPark/HaParkApplication.java
@@ -0,0 +1,13 @@
+package com.team5.HAPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaParkApplication.class, args);
+	}
+
+}
---
+++ HAPark/src/test/java/com/team5/HAPark/HaParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.HAPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
