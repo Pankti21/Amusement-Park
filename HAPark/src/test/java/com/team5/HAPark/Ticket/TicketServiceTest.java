52bdab9b7b82ed91b7aca133f189d6f2be091b0e
Shelja <sh286380@dal.ca>
2021-11-28 13:14:01 -0600

    Adding more test cases for Ticket Service class

--- HAPark/src/test/java/com/team5/HAPark/Ticket/TicketServiceTest.java
+++ HAPark/src/test/java/com/team5/HAPark/Ticket/TicketServiceTest.java
@@ -1,9 +1,5 @@
 package com.team5.HAPark.Ticket;
 
-import com.team5.HAPark.Ride.Model.Ride;
-import com.team5.HAPark.Ticket.*;
-import com.team5.HAPark.Ticket.Ticket;
-import com.team5.HAPark.Ticket.TicketOrderItem;
 import com.team5.HAPark.Ticket.DAO.ITicketPersistence;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -23,29 +19,50 @@ class TicketServiceTest {
     static TicketOrderItem ticketOrderItem;
     static Ticket ticket1;
     static Ticket ticket2;
+    static Ticket ticket3;
+    static Ticket ticket4;
     static List<Ticket> tickets = new ArrayList<>();
 
    @BeforeAll
     static void init() throws SQLException {
         ticket1 = new Ticket("Child",10);
         ticket2 = new Ticket("Adult",15);
+        ticket3 = new Ticket("Adult",15);
         ticketOrderItem = new TicketOrderItem(ticket1,2);
         ITicketPersistence ticketPersistenceMock = Mockito.mock(ITicketPersistence.class);
         Mockito.when(ticketPersistenceMock.loadTicket("Child")).thenReturn(ticket1);
+       Mockito.when(ticketPersistenceMock.loadTicket("Adult")).thenReturn(ticket2);
         ticketService = new TicketService(ticketPersistenceMock);
         tickets.add(ticket1);
         tickets.add(ticket2);
+
         Mockito.when(ticketPersistenceMock.getAllTickets()).thenReturn(tickets);
     }
 
     @Test
-    void getTicket() throws SQLException {
+    void getChildTicket() throws SQLException {
         assertEquals(ticket1,ticketService.getTicket("Child"));
     }
 
     @Test
+    void getAdultTicket() throws SQLException {
+        assertEquals(ticket2,ticketService.getTicket("Adult"));
+    }
+    @Test
     void getAllTickets() throws  SQLException {
-        assertEquals(2,ticketService.getAllTickets().size());
+        assertEquals(4,ticketService.getAllTickets().size());
     }
 
+    @Test
+    void getAllMoreTickets() throws  SQLException {
+        tickets.add(ticket3);
+        assertEquals(5,ticketService.getAllTickets().size());
+    }
+
+    @Test
+    void getAllFiveTickets() throws  SQLException {
+        tickets.add(ticket3);
+        tickets.add(ticket4);
+        assertEquals(4,ticketService.getAllTickets().size());
+    }
 }
