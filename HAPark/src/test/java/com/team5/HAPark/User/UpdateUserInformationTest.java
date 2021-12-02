bad1ef4f73e0ed75ea44a6b68ec8b48d086c012d
Shelja <sh286380@dal.ca>
2021-12-02 17:07:14 -0600

    Modified test cases in UpdateUserInformationTest

--- HAPark/src/test/java/com/team5/HAPark/User/UpdateUserInformationTest.java
+++ HAPark/src/test/java/com/team5/HAPark/User/UpdateUserInformationTest.java
@@ -1,5 +1,5 @@
- //package com.team5.HAPark.User;
-/*
+package com.team5.HAPark.User;
+
 import com.team5.HAPark.User.DAO.IUserPersistence;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
@@ -10,46 +10,43 @@ import java.security.NoSuchAlgorithmException;
 import org.junit.jupiter.api.Test;
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class UpdateUserInformationTest {
 
     private static UpdateUserInformation updateUserInformation;
     private static User user;
-    private static User user1;
     private static IUserPersistence userPersistenceMock;
 
     @BeforeEach
-    void init(){
-        user1 = new User();
-        user1.setPassword("OldPassword2@123");
-        user1.setConfirmedPassword("NewPassword2@123");
-        user1.setReconfirmedPassword("NewPassword2@123");
-        updateUserInformation = new UpdateUserInformation(user1);
+    void init() {
         user = new User();
-        user.setPassword("OldPassword@123");
         user.setConfirmedPassword("NewPassword@123");
         user.setReconfirmedPassword("NewPassword@123");
         user.setEmail("test@gmail.com");
+        user.setPassword("password");
         updateUserInformation = new UpdateUserInformation(user);
         userPersistenceMock = Mockito.mock(IUserPersistence.class);
     }
-
     @Test
-    @WithMockUser(username = "user123",password = "password")
+    @WithMockUser(username = "test@gmail.com")
     public void validateUpdatedUserPassword() throws SQLException, NoSuchAlgorithmException {
-        assertEquals(false,
-                updateUserInformation.updateUserPassword
+        String oldPassword = Encryption.encryptPassword("password");
+        when(userPersistenceMock.getPassword("test@gmail.com")).thenReturn(oldPassword);
+        assertFalse( updateUserInformation.updateUserPassword
                         (userPersistenceMock, user.getPassword(),user.getConfirmedPassword(), user.getReconfirmedPassword()));
     }
 
     @Test
-    @WithMockUser(username = "user123",password = "password")
-    public void validUpdatedUserPassword2() throws SQLException, NoSuchAlgorithmException {
-        //System.out.println("password :" + user1.getPassword());
-        assertEquals(false,
-                updateUserInformation.updateUserPassword
-                        (userPersistenceMock, user1.getPassword(),user1.getConfirmedPassword(), user1.getReconfirmedPassword()));
+    @WithMockUser(username = "test@gmail.com")
+    public void validateUpdatedOtherUserPassword() throws SQLException, NoSuchAlgorithmException {
+        user.setPassword("otherPassword");
+        String oldPassword = Encryption.encryptPassword("otherPassword");
+        when(userPersistenceMock.getPassword("test@gmail.com")).thenReturn(oldPassword);
+        assertFalse( updateUserInformation.updateUserPassword
+                (userPersistenceMock, user.getPassword(),user.getConfirmedPassword(), user.getReconfirmedPassword()));
     }
-}*/
+
+}
