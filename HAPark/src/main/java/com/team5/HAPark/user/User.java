9dcb8ed707d24da638ebd809cf9421e6482f0c10
Leah Isenor <leah.isenor@dal.ca>
2021-12-03 22:20:19 -0400

    Extracting subclasses from user (work in progress)

--- HAPark/src/main/java/com/team5/HAPark/user/User.java
+++ HAPark/src/main/java/com/team5/HAPark/user/User.java
@@ -2,39 +2,14 @@ package com.team5.HAPark.user;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
     private String email;
     private String password;
-    private String confirmedPassword;
-    private String reconfirmedPassword;
+
     public User(){}
 
-    public User(String confirmedPassword, String reconfirmedPassword) {
-        this.confirmedPassword = confirmedPassword;
-        this.reconfirmedPassword = reconfirmedPassword;
-    }
-    public User(String firstName, String lastName, String email, String password){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public User(String email,String password){
         this.email = email;
         this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -53,19 +28,4 @@ public class User {
         this.password = password;
     }
 
-    public String getConfirmedPassword() {
-        return confirmedPassword;
-    }
-
-    public void setConfirmedPassword(String confirmedPassword) {
-        this.confirmedPassword = confirmedPassword;
-    }
-
-    public String getReconfirmedPassword() {
-        return reconfirmedPassword;
-    }
-
-    public void setReconfirmedPassword(String reconfirmedPassword) {
-        this.reconfirmedPassword = reconfirmedPassword;
-    }
 }
