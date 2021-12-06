4c44a6906f17f7bad432deefd0131ee7ce03d8f9
Leah Isenor <leah.isenor@dal.ca>
2021-12-06 11:52:38 -0400

    Moved tests to correct class

--- HAPark/src/main/java/com/team5/HAPark/user/model/UpdateUserInformation.java
+++ HAPark/src/main/java/com/team5/HAPark/user/model/UpdateUserInformation.java
@@ -25,7 +25,6 @@ public class UpdateUserInformation implements IUpdateUserInformation {
             if (isNewPasswordDifferent()) {
                 if (newPasswordMatchesConfirmedPassword()) {
                     if (emailPasswordValidation.validatePasswordFormat()) {
-                        String newPasswordEncrypted = Encryption.encryptPassword(user.getPassword());
                         userPersistence.userUpdatedPassword(user.getPassword(), user.getEmail());
                         return UpdateUserValidationResult.SUCCESSFUL;
                     } else {
--- HAPark/src/test/java/com/team5/HAPark/ticket/TicketOrderItemTest.java
+++ HAPark/src/test/java/com/team5/HAPark/ticket/TicketOrderItemTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TicketOrderItemTest {
+
     private TicketOrderItem ticketOrderItem;
+    private Ticket ticket2;
 
     @BeforeEach
     void setUp() {
         Ticket ticket = new Ticket("item",15);
         ticketOrderItem = new TicketOrderItem(ticket,3);
+        ticket2 = new Ticket("item",15.25);
     }
 
     @Test
@@ -36,6 +39,17 @@ class TicketOrderItemTest {
     void setTicketPrice() {
         ticketOrderItem.setTicketPrice(5);
         assertEquals(5,ticketOrderItem.getTicketPrice());
+    }
+
+    @Test
+    void getTotalPriceWithIntegerPrice() {
+        assertEquals(45,ticketOrderItem.getTotalPrice());
+    }
+
+    @Test
+    void getTotalPriceWithDecimalPrice() {
+        ticketOrderItem = new TicketOrderItem(ticket2,3);
+        assertEquals(45.75,ticketOrderItem.getTotalPrice());
     }
 
 }
